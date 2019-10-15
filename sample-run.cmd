dc86c44767ed731ab3e15828e86302f827539686
jazzwang <jazzwang.tw@gmail.com>
2019-10-15 10:10:22 +0800

    sample windows script

---
+++ sample-run.cmd
@@ -0,0 +1,6 @@
+set HOME_URL=https://cwiki.apache.org/confluence
+set BASE_URL=https://cwiki.apache.org
+set SPACE_KEY=HADOOP
+python3 get-pages.py
+python3 get-pageIds.py
+python3 get-pageHistories.py
