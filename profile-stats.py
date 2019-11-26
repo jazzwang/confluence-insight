58cfdb3f5b8c7cb490581127f9970814018461b0
jazzwang <jazzwang.tw@gmail.com>
2019-11-26 11:54:33 +0800

    draft script to analyze wiki user profile

---
+++ profile-stats.py
@@ -0,0 +1,33 @@
+#!env python
+# -*- coding: utf-8 -*-
+
+import os
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+if len(sys.argv) < 2:
+    print("Usage: " + sys.argv[0] + " WIKI_URL")
+    sys.exit(1)
+
+driver = webdriver.Chrome()
+driver.set_page_load_timeout(30)
+driver.get(sys.argv[1])
+
+# Recursive condition to expand all closed tree
+more_pages = True
+while more_pages:
+    try:
+        node = driver.find_element_by_class_name("more-link")
+        node.find_element_by_class_name("more-link").click()
+        time.sleep(1)
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    except:
+        more_pages = False
+
+soup = BeautifulSoup(driver.page_source,"lxml")
+print(soup.prettify())
+
+driver.quit()
