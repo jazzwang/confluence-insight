14a30096350a51781b06fdc61b5b78b2b00340ff
jazzwang <jazzwang.tw@gmail.com>
2020-12-20 23:27:41 +0800

    update sample-output & sample-run.sh

--- bin/sample-run.sh
+++ bin/sample-run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export HOME_URL=https://cwiki.apache.org/confluence
 export BASE_URL=https://cwiki.apache.org
-export SPACE_KEY=HADOOP
+if [ "$1" == "" ]; then
+    export SPACE_KEY=HADOOP
+else
+    export SPACE_KEY=$1
+fi
 python3 get-pages.py
 python3 get-pageIds.py
 python3 get-pageHistories.py
